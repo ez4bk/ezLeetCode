cab645dd7ac9e926619322a79b0b8a9c7fcd121d
ez4bk <qq1034351717@gmail.com>
2023-07-06 12:02:51 +0800

    O(1) space find error number

---
+++ 645.set-mismatch.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=645 lang=python3
+#
+# [645] Set Mismatch
+#
+
+
+# @lc code=start
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        dup = -1
+        missing = 1
+        for n in nums:
+            if nums[abs(n) - 1] < 0:
+                dup = abs(n)
+            else:
+                nums[abs(n) - 1] *= -1
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                missing = i + 1
+        return [dup, missing]
+
+
+# @lc code=end
