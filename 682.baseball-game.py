3382f5ebb4ed08216759f2f7aac1927815c6187e
ez4bk <qq1034351717@gmail.com>
2023-07-06 12:59:14 +0800

    ez baseball

---
+++ 682.baseball-game.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=682 lang=python3
+#
+# [682] Baseball Game
+#
+
+
+# @lc code=start
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        score_board = []
+        for op in operations:
+            if op == "+":
+                score_board.append(score_board[-1] + score_board[-2])
+            elif op == "D":
+                score_board.append(score_board[-1] * 2)
+            elif op == "C":
+                score_board.pop()
+            else:
+                score_board.append(int(op))
+
+        return sum(score_board)
+
+
+# @lc code=end
