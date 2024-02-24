e359084688d28949e775c48b1d0c6c07bedde301
Yuchen Wei <86708505+ez4bk@users.noreply.github.com>
2024-02-23 21:39:13 -0500

    Stack

---
+++ Neetcode/Stack/20.valid-parentheses.py
@@ -0,0 +1,35 @@
+#
+# @lc app=leetcode id=20 lang=python3
+#
+# [20] Valid Parentheses
+#
+
+# @lc code=start
+class Solution:
+
+    def isValid(self, s: str) -> bool:
+        partner = {
+            '(': ')',
+            '[': ']',
+            '{': '}'
+        }
+        topIndex = -1
+        stack = []
+        
+        for c in s:
+            if c in partner.keys():
+                stack.append(c)
+                topIndex += 1
+            else:
+                if stack and partner[stack[topIndex]] == c:
+                    stack.pop(topIndex)
+                    topIndex -= 1
+                else:
+                    return False
+        
+        if stack:
+            return False
+        
+        return True
+
+# @lc code=end
